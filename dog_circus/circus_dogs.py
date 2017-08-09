9bf06a13c2ed3b91c80094988aa84ea596752cc3
George Kaimakis <geomatus@mac.com>
2017-08-09 13:44:27 +0100

    Adds Class variables:
    'circus_dogs' list keeps track of instances created
    in terms of self.name.
    'num_of_dogs' keeps track of number of created instances
    by incrementing with each __init__ call.

--- dog_circus/circus_dogs.py
+++ dog_circus/circus_dogs.py
@@ -7,12 +7,19 @@
 class CircusDogs(object):
     """Create circus dog class objects"""
 
+    circus_dogs = []
+    num_of_dogs = 0
+
     def __init__(self, name, breed):
         """create instance with name, breed and empty trick-list"""
         self.name = name
         self.breed = breed
         self.tricks = []
         self.trick_counter = 0
+        # increment number of instances created
+        CircusDogs.num_of_dogs += 1
+        # add created instances to list of dogs
+        CircusDogs.circus_dogs.append(self.name)
 
     def get_name(self):
         """return name"""
